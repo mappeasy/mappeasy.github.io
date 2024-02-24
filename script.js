f28efd98132c9123621f7a2d654f440303f9f412
mappeasy <147260512+mappeasy@users.noreply.github.com>
2024-02-23 18:17:27 -0600

    fixed makeDraggle

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ async function queryFeaturesByEndPoint(mapServer, unitID) {
         return null;
     }
 }
-
 async function queryFeature2AtFeature1(mapServerFeature1, mapServerFeature2, unitID, outfieldLine) {
     // Step 1: Query to get the endpoint of the line
     let endPoint = null;
@@ -340,29 +339,29 @@ function calculateRoute(data) {
     let total_hour_max = roundToDecimals(toal_second_max / 3600, 2);
     return {total_len_ft,total_len_mi,total_hour_min,total_hour_max};
 }
-    function makeDraggable(dragHandle, draggableElement) {
-        var dragHandle = document.getElementById(dragHandle);
-        var draggableElement = document.getElementById(draggableElement);
-        var offsetX, offsetY, initialMouseX, initialMouseY;
+function makeDraggable(dragHandle, draggableElement) {
+    var dragHandle = document.getElementById(dragHandle);
+    var draggableElement = document.getElementById(draggableElement);
+    var offsetX, offsetY, initialMouseX, initialMouseY;
 
-        dragHandle.addEventListener('mousedown', function(e) {
-            offsetX = draggableElement.offsetLeft;
-            offsetY = draggableElement.offsetTop;
-            initialMouseX = e.clientX;
-            initialMouseY = e.clientY;
+    dragHandle.addEventListener('mousedown', function(e) {
+        offsetX = draggableElement.offsetLeft;
+        offsetY = draggableElement.offsetTop;
+        initialMouseX = e.clientX;
+        initialMouseY = e.clientY;
 
-            document.addEventListener('mousemove', onMouseMove);
-            document.addEventListener('mouseup', onMouseUp);
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
 
-            e.preventDefault(); // Prevents text selection during drag
-        });
-        function onMouseMove(e) {
-            draggableElement.style.left = offsetX + e.clientX - initialMouseX + 'px';
-            draggableElement.style.top = offsetY + e.clientY - initialMouseY + 'px';
-        }
-
-        function onMouseUp() {
-            document.removeEventListener('mousemove', onMouseMove);
-            document.removeEventListener('mouseup', onMouseUp);
-        }
+        e.preventDefault(); // Prevents text selection during drag
+    });
+    function onMouseMove(e) {
+        draggableElement.style.left = offsetX + e.clientX - initialMouseX + 'px';
+        draggableElement.style.top = offsetY + e.clientY - initialMouseY + 'px';
     }
+
+    function onMouseUp() {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+    }
+}
