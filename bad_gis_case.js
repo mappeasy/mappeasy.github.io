d52a0984cb52c0e23bb0e577a39d4895b01fe228
mappeasy <147260512+mappeasy@users.noreply.github.com>
2024-03-12 00:32:10 -0500

    Update bad_gis_case.js

--- bad_gis_case.js
+++ bad_gis_case.js
@@ -1,4 +1,8 @@
+//if current unitid is unable to identify the trace, then choose the correct unitid
+//for example: if unitid 2 of the current line is: 2361191; it's not the unitid of the next line
+//then choose the correct line with unitid: 7853045
 var bad_gis_case = {
-    '2361191': '7853045'
+    '2361191': '7853045',
+    '4153751' : '4188067'
     // Add more pairs if needed - replace key by value
 };
